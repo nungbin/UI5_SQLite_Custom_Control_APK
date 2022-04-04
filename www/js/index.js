fb529162b6400df1fa245abdfba1a12bd3e121a9
Bin Shu <shubinlin@gmail.com>
2022-04-03 20:54:25 -0600

    add sqlite logic to index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,98 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+$(document).ready(function(){
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
-
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+var myDB;
+//Open Database Connection
+document.addEventListener("deviceready",onDeviceReady,false);
+function onDeviceReady(){
+myDB = window.sqlitePlugin.openDatabase({name: "mySQLite.db", location: 'default'});
+window.sqliteDB = myDB;
+    
+alert("Device Ready Event");
 }
+//Create new table
+$("#createTable").click(function(){
+    myDB.transaction(function(transaction) {
+    transaction.executeSql('CREATE TABLE IF NOT EXISTS phonegap_pro (id integer primary key, title text, desc text)', [],
+        function(tx, result) {
+            alert("Table created successfully");
+        }, 
+        function(error) {
+              alert("Error occurred while creating the table.");
+        });
+    });
+});
+
+//Insert New Data
+$("#insert").click(function(){
+  var title=$("#title").val();
+  var desc=$("#desc").val();
+  console.log(title +""+ desc);
+  myDB.transaction(function(transaction) {
+        var executeQuery = "INSERT INTO phonegap_pro (title, desc) VALUES (?,?)";             
+        transaction.executeSql(executeQuery, [title,desc]
+            , function(tx, result) {
+                 alert('Inserted');
+            },
+            function(error){
+                 alert('Error occurred'); 
+            });
+    });
+});
+
+//Display Table Data
+$("#showTable").click(function(){
+  $("#TableData").html("");
+  myDB.transaction(function(transaction) {
+  transaction.executeSql('SELECT * FROM phonegap_pro', [], function (tx, results) {
+       var len = results.rows.length, i;
+       $("#rowCount").html(len);
+       for (i = 0; i < len; i++){
+          $("#TableData").append("<tr><td>"+results.rows.item(i).id+"</td><td>"+results.rows.item(i).title+"</td><td>"+results.rows.item(i).desc+"</td><td><a href='edit.html?id="+results.rows.item(i).id+"&title="+results.rows.item(i).title+"&desc="+results.rows.item(i).desc+"'>Edit</a> &nbsp;&nbsp; <a class='delete' href='#' id='"+results.rows.item(i).id+"'>Delete</a></td></tr>");
+       }
+    }, null);
+  });
+});
+
+//Delete Data from Database
+$(document.body).on('click', '.delete' ,function(){
+  var id=this.id;
+  myDB.transaction(function(transaction) {
+    var executeQuery = "DELETE FROM phonegap_pro where id=?";
+    transaction.executeSql(executeQuery, [id],
+      //On Success
+      function(tx, result) {alert('Delete successfully');},
+      //On Error
+      function(error){alert('Something went Wrong');});
+  });
+});
+
+
+//Delete Tables
+$("#update").click(function(){
+  var id=$("#id").text();
+  var title=$("#title").val();
+  var desc=$("#desc").val()
+  myDB.transaction(function(transaction) {
+    var executeQuery = "UPDATE phonegap_pro SET title=?, desc=? WHERE id=?";
+    transaction.executeSql(executeQuery, [title,desc,id],
+      //On Success
+      function(tx, result) {alert('Updated successfully');},
+      //On Error
+      function(error){alert('Something went Wrong');});
+  });
+});
+
+$("#DropTable").click(function(){
+    myDB.transaction(function(transaction) {
+        var executeQuery = "DROP TABLE  IF EXISTS phonegap_pro";
+        transaction.executeSql(executeQuery, [],
+            function(tx, result) {alert('Table deleted successfully.');},
+            function(error){alert('Error occurred while droping the table.');}
+        );
+    });
+});
+
+});
+
+
+
